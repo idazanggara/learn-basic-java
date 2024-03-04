3b76980f1a0d17b39ef105259ef2720b54fce12d
idazanggara <idazideas@gmail.com>
2024-03-04 13:44:13 +0700

    Belajar Polymorphism

---
+++ src/JavaOOPPolymorphism/polymorphism/Developer.java
@@ -0,0 +1,16 @@
+package JavaOOPPolymorphism.polymorphism;
+
+public class Developer extends Employee{
+    public Developer(String name, String division, Long salary) {
+        super(name, division, salary);
+    }
+
+    @Override
+    public String print() {
+        return "Developer{"+
+                "name='" + name + "'," +
+                "division='" + division + "'," +
+                "salary='" + salary + "'"+
+                "}";
+    }
+}
